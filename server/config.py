667e90371ceb85db85fa57baf7ac8b01dee39046
agark0vlad21 <72999843+agark0vlad21@users.noreply.github.com>
2022-12-26 11:31:58 +0000

    Update config.py

--- server/config.py
+++ server/config.py
@@ -3,5 +3,7 @@ HOST = "" # adress to bind
 MULTIVATE_METHOD = "thread" # method of multivate, avaliabe only thread and process
 CLI_COMMANDS = {
         "connected": "print(len(sockets))",
-        "help": "for i in list(CLI_COMMANDS):\n    print(i)"
+        "help": "print ('Avaliable commands:')\nfor i in list(CLI_COMMANDS):\n    print(i)",
+        "kickall": "for sock in sockets:\n    sock.close()"
         }
+KICK_WORDS = []
