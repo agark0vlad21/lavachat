0ff824aec164638c5b7dddcac8b082160dcadd5c
agark0vlad21 <vladiscas@gmail.com>
2023-02-03 18:09:39 +0300

    Updated lavachat client
    Now lavachat client can parse command line arguments

--- client/config.py
+++ client/config.py
@@ -1,3 +1,3 @@
-PORT = 7777 # Port for connection
-SERVER = "127.0.0.1" # Ip of server for connect
-RETRIES = 10 # Connection retries number
+Port = 7777  # Port for connection
+Adress = "127.0.0.1"  # IP of server for connect
+Retries = 10  # Connection retries number
--- client/main.py
+++ client/main.py
@@ -1,36 +1,69 @@
-from sys import exit
-import socket
+from socket import socket, AF_INET, SOCK_STREAM
 from threading import Thread
 from time import sleep as wait
-from config import PORT, RETRIES, SERVER
+from config import Port as ConfPort, Retries as ConfRetries, Adress as ConfAdress
+from atexit import register
+from argparse import ArgumentParser
+from sys import exit
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+parser = ArgumentParser(description="Simple chat in terminal")
+parser.add_argument("-p",
+                    "--port",
+                    type=int,
+                    help=f"Port for connection (default is {ConfPort})",
+                    default=ConfPort)
+parser.add_argument("-a",
+                    "--adress",
+                    type=str,
+                    help=f"Adress for connecting (default is {ConfAdress})",
+                    default=ConfAdress)
+parser.add_argument("-r",
+                    "--retries",
+                    type=int,
+                    help=f"Retries for connecting (default is {ConfRetries})",
+                    default=ConfRetries)
+args, unknown = parser.parse_known_args()
+if len(unknown) != 0:
+    print(f"Unknown args: {unknown}")
+
+
+client = socket(AF_INET, SOCK_STREAM)
 
 try:
-    client.connect((SERVER, PORT))
-except ConnectionRefusedError:
-    for i in range(0, RETRIES):
-        i += 1
-        print(f"attempt {i} to reconnect")
-        try:
-            client.connect((SERVER, PORT))
-            break
-        except ConnectionRefusedError:
-            if i == RETRIES + 1:
-                exit()
-            wait(10)
+    client.connect((args.adress, args.port))
+except OSError:
+    try:
+        for i in range(0, args.retries):
+            i += 1
+            print(f"Attempt {i} to reconnect")
+            try:
+                client.connect((args.adress, args.port))
+                break
+            except OSError:
+                if i == args.retries + 1:
+                    exit()
+                wait(10)
+    except KeyboardInterrupt:
+        print("\rCancelled by user, exiting")
+        exit()
+
 print("Connected!")
+
+
+# Registering close connection function at exit
+@register
+def close_at_exit():
+    client.close()
 
 
 def reader():
     while True:
-        data = client.recv(4096)
-        if data.decode() != "":
-            print(repr(data.decode()))
+        msg = client.recv(4096).decode()
+        if msg != "":
+            print(repr(msg))
         else:
             print("Connection closed")
-            client.close()
-            exit(0)
+            sender.exit()
             break
 
 
@@ -42,9 +75,8 @@ def sender():
 Thread(target=sender, daemon=True).start()
 Thread(target=reader, daemon=True).start()
 
-# Infinity cycle for do nothing
-try:
-    while True:
+while True:
+    try:
         wait(10)
-except KeyboardInterrupt:
-    exit()
+    except KeyboardInterrupt:
+        exit()
