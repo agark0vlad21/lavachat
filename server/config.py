fb046fd5a412c200379d65a68ed3f2bb5bd2dd11
agark0vlad21 <vladiscas@gmail.com>
2023-02-15 00:08:19 +0300

    Config update

--- server/config.py
+++ server/config.py
@@ -1,6 +1,10 @@
-Port = 7777  # port to listen
-Host = ""  # adress to bind
-MultivateMethod = "Thread"  # method of multivate, avaliabe only thread and process
+# [Server]
+Port = 7777  # Port to listen
+Host = ""  # Adress to bind
+MultivateMethod = "Thread"  # Method of multivate, avaliabe only thread and process
+Protocol' = "TCP"  # Protocol for lavachat (UDP faster but can miss some data, TCP slower but guarantees data transfer)
+
+# [Cli]
 CliCommands = {
         "connected": "print(len(sockets))",
         "help": "print ('Avaliable commands:')\nfor i in list(CliCommands):\n    print(i)",
